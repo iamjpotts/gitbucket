a2caaf073af0aefe733e1904d58e13ed39ac7125
Joshua Potts <8704475+iamjpotts@users.noreply.github.com>
2026-05-25 15:44:33 -0400

    Adjust style of comment on CreateAFork

--- src/main/scala/gitbucket/core/api/CreateAFork.scala
+++ src/main/scala/gitbucket/core/api/CreateAFork.scala
@@ -1,4 +1,7 @@
 package gitbucket.core.api
 
-// https://docs.github.com/en/rest/repos/forks#create-a-fork
+/**
+ * https://docs.github.com/en/rest/repos/forks#create-a-fork
+ * api form
+ */
 case class CreateAFork(organization: Option[String])
